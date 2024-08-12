46b3927cbb20d7aacbd48be2511b903f429ead89
Anshika Puri <anshikapurilearner@gmail.com>
2024-08-12 12:05:07 +0530

    fixing-errors

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,19 +1,25 @@
 import React from 'react'
 
 const NavLinks = ({data,activeSection}) => {
+   const handleClick = (id) => {
+    const section = document.getElementById(id);
+    section.scrollIntoView({ behavior: 'smooth', block: 'start'  });
+  };
   return (
     <ul className='navlist'>
     {data.length > 0 ? (
-      data.map((item) => (
-        <li key={item.id}>
+      data.map((item) => {
+        const sectionId = item.category.toLowerCase().replace(/ /g, '-');
+       return <li key={item.id}>
           <a
             href={`#${item.category.toLowerCase().replace(/ /g, '-')}`}
             className={activeSection === item.category.toLowerCase().replace(/ /g, '-') ? 'active' : ''}
+            onClick={() => handleClick(sectionId)}
           >
             {item.category}
           </a>
         </li>
-      ))
+})
     ) : (
       <li>No services available</li>
     )}
